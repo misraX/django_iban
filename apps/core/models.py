ff3b5ea8eadc169ca8e584e64b2d61cd63647fe4
misraX <fordax05@gmail.com>
2018-02-28 07:46:24 +0200

    Enhancing core model documentation.

--- apps/core/models.py
+++ apps/core/models.py
@@ -8,12 +8,26 @@ from django.db import models
 
 class AbstractBaseModel(models.Model):
     """
-    Base model with most used fields.
+    Base db model with most used fields.
+
     :uuid `UUIDField`, defines a unique instance uuid version 4.
+
     :created_at `DateTimeField`, defines instance creation date.
+
     :updated_at `DateTimeField`, defines instance updating date.
+
+    Example:
+
+    ..  code-block:: python
+
+        from django.db import models
+        from apps.core.model import AbstractBaseModel
+
+        class MyNewModel(AbstractBaseModel):
+            name = models.CharField(max_length=255)
+
     """
-    uuid = models.UUIDField(default=uuid.uuid4())
+    uuid = models.UUIDField(default=uuid.uuid4(), editable=False)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
--- bank_accounts/settings.py
+++ bank_accounts/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'bank_accounts.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'template/'), ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
