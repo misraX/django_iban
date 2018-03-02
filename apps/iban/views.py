2022081084b49120650bf179a1421df33a4fd252
misraX <fordax05@gmail.com>
2018-03-02 07:34:37 +0200

    Adding PreventManipulationAccessMixin, and changes Views.
    PreventManipulationAccessMixin is a new AccessMixin, which
    will act as LoginRequiredMixin and restrict access on the
    view only for who created the object.
    
    Changes View configuration, code refactoring, enhancing docs
    and new mixins to handle the Views, PreventManipulationAccessMixin
    will only used with Update and Delete View, all the other Views
    have a LoginRequired mixins which prevents Anonymous request to the
    website.

--- apps/iban/views.py
+++ apps/iban/views.py
@@ -1,116 +1,107 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
-from __future__ import unicode_literals, absolute_import
+from __future__ import unicode_literals
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import HttpResponse
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import (
     ListView, DetailView, DeleteView, UpdateView, CreateView
 )
 
+from .auth.mixins import PreventManipulationAccessMixin
 from .forms import IBANAccountModelForm
 from .models import IBANAccount
 
 
-class IBANBaseSingleObjectView(LoginRequiredMixin):
+class IBANBaseViewConfiguration(object):
     """
-    Extends `LoginRequiredMixin` to handle the base
-    configuration for generic List, Detail views.
+    The very basic configuration for IBAN Views.
     """
     model = IBANAccount
+
+
+class IBANBaseListDetailView(LoginRequiredMixin, IBANBaseViewConfiguration):
+    """
+    Base List and Detail Views, extends `LoginRequiredMixin`
+    and `IBANBaseViewConfiguration` to handle authorization
+    and base configuration for generic List, Detail Views.
+    """
+
     # use the active manager as the default manager.
-    queryset = IBANAccount.is_active.all()
-    context_object_name = 'iban_account_item'
-    redirect_field_name = 'redirect_to'
+    queryset = IBANAccount.objects.active()
 
 
-class IBANBaseSingleObjectUpdateCreateView(LoginRequiredMixin):
+class IANBaseCreateUpdateView(IBANBaseViewConfiguration):
     """
-    Extends `LoginRequiredMixin` to handle the base
-    configuration for generic Update, Create views.
+    Base Create and Update Views, extends `IBANBaseViewConfiguration`
+    mainly used explicitly with Update View since Update View and in
+    combination with `LoginRequiredMixin` in IBANBaseCreateView, as
+    Update View handles authorization differently.
     """
-    model = IBANAccount
-    context_object_name = 'iban_account_item'
+    success_url = reverse_lazy('iban_list')
     template_name = 'ibanaccount_form.html'
     form_class = IBANAccountModelForm
-    redirect_field_name = 'redirect_to'
+
+    def form_valid(self, form):
+        """
+        Assign the form model instance ForeignKey field
+        `created_by` with the request.user.
+
+        :param form: ModelForm
+        :return: HttpResponseRedirect
+        """
+        form.instance.created_by = self.request.user
+        return super(IANBaseCreateUpdateView, self).form_valid(form)
 
 
-class IBANListView(IBANBaseSingleObjectView, ListView):
+class IBANBaseCreateView(LoginRequiredMixin, IANBaseCreateUpdateView):
     """
-    List all IBAN instances
+    Base CreateView extends `LoginRequiredMixin` and
+    `IANBaseCreateUpdateView` to handle login required
+    Authorization and base View configuration.
+    """
+    pass
+
+
+class IBANListView(IBANBaseListDetailView, ListView):
+    """
+    List all `IBANAccount` instances, with 10 pagination
+    limit.
     """
     paginate_by = 10
     context_object_name = 'iban_account_list'
     template_name = 'ibanaccount_list.html'
 
 
-class IBANDetailView(IBANBaseSingleObjectView, DetailView):
+class IBANDetailView(IBANBaseListDetailView, DetailView):
     """
-    `IBANAccount` detail view.
+    Detail an `IBANAccount` instance by `pk`.
     """
+    context_object_name = 'iban_account_item'
     template_name = 'ibanaccount_detail.html'
 
 
-class IBANDeleteView(IBANBaseSingleObjectView, DeleteView):
+class IBANCreateView(IBANBaseCreateView, CreateView):
+    pass
+
+
+class IBANUpdateView(PreventManipulationAccessMixin, IANBaseCreateUpdateView, UpdateView):
     """
-    `IBANAccount` delete view.
+    Update `IBANAccount` View, extends `PreventManipulationAccessMixin`
+    `IANBaseCreateUpdateView` and generic `UpdateView`, a restricted
+    View that prevent not only Anonymous users but also Authorized
+    users from performing any operation on the model instance, unless
+    it's owned by the request.user.
     """
-    success_url = '/'
+    pass
+
+
+class IBANDeleteView(PreventManipulationAccessMixin, IBANBaseViewConfiguration, DeleteView):
+    """
+    Delete `IBANAccount` View, extends `PreventManipulationAccessMixin`
+    `IANBaseCreateUpdateView` and generic `DeleteView`, a restricted
+    View that prevent not only Anonymous users but also Authorized
+    users who did not create the model instance from performing any
+    operation on the model instance.
+    """
     template_name = 'ibanaccount_delete.html'
-
-    def dispatch(self, request, *args, **kwargs):
-        """
-        Preventing manipulations operations over the model
-        instance and allowing it only for users who created it.
-
-        :param request: HttpRequest
-        :param args:
-        :param kwargs:
-        :return: HttpResponse
-        """
-        super(IBANDeleteView, self).dispatch(request, *args, **kwargs)
-        if self.get_object().created_by != self.request.user:
-            return HttpResponse('Not authorized', status=403)
-
-
-class IBANCreateView(IBANBaseSingleObjectUpdateCreateView, CreateView):
-
-    def form_valid(self, form):
-        """
-        Auto assign the form model instance ForeignKey
-        field `created_by` with the request.user.
-
-        :param form: ModelForm
-        :return: HttpResponseRedirect
-        """
-        form.instance.created_by = self.request.user
-        return super(IBANCreateView, self).form_valid(form)
-
-
-class IBANUpdateView(IBANBaseSingleObjectUpdateCreateView, UpdateView):
-
-    def form_valid(self, form):
-        """
-        Auto assign the form model instance ForeignKey
-        field `created_by` with the request.user.
-
-        :param form: ModelForm
-        :return: HttpResponseRedirect
-        """
-        form.instance.created_by = self.request.user
-        return super(IBANUpdateView, self).form_valid(form)
-
-    def dispatch(self, request, *args, **kwargs):
-        """
-        Preventing manipulations operations over the model
-        instance and allowing it only for users who created it.
-
-        :param request: HttpRequest
-        :param args:
-        :param kwargs:
-        :return: HttpResponse
-        """
-        super(IBANUpdateView, self).dispatch(request, *args, **kwargs)
-        if self.get_object().created_by != self.request.user:
-            return HttpResponse('Not authorized', status=403)
+    success_url = reverse_lazy('iban_list')
